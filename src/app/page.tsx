2d5ef06628aeae32eecf276e82cf7c5101ac81ca
Fonte Fy <fyfonte@gmail.com>
2025-11-07 23:51:42 +0000

    O cabeçalho deve ser transparente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-dvh bg-background text-foreground">
-      <header className="fixed top-0 left-0 right-0 z-50 bg-black/30 backdrop-blur-lg">
+      <header className="fixed top-0 left-0 right-0 z-50">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex items-center space-x-4">
