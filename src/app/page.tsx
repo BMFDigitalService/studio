35e15c8879978c7f02dd865bdede5b5a2983f586
Fonte Fy <fyfonte@gmail.com>
2025-11-08 00:31:56 +0000

    Em baixo no lugar de © 2025 Albino Prestação de Serviços. Todos os direi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
       
       <footer className="bg-secondary py-8">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center text-secondary-foreground">
-            <p>&copy; {new Date().getFullYear()} Albino Prestação de Serviços. Todos os direitos reservados.</p>
+            <p>Copyright© 3dabliu. Todos os direitos reservados.</p>
           </div>
       </footer>
     </div>
